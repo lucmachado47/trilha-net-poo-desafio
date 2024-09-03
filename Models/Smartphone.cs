26ddbbc9679b2970433777f8daf19fdd3382ce03
Lucas Machado <lucas_machado47@outlook.com>
2024-09-02 21:29:49 -0300

    desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,56 @@
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
-    {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+    {   
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        public string Numero { get; set; }
+        private string Modelo { 
+            get => _modelo; 
+
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O valor não pode ser vazio");
+                }
+                _modelo = value;
+            } 
+            }
+        private string IMEI { 
+            get => _imei; 
+
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("O valor não pode ser vazio");
+                }
+                _imei = value;
+            } 
+            }
+        private int Memoria { 
+            get => _memoria; 
+
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("O valor não pode ser negativo");
+                }
+                _memoria = value;
+            } 
+            }
+        
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "99556677", modelo: "Tijolão", imei: "112233", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "99665544", modelo: "15 Pro", imei: "223311", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
